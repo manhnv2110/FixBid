81f90bd974defc57565a2e4f69649352fab87605
NgoTheTan <23021691@vnu.edu.vn>
2026-05-03 10:18:15 +0700

    update dependencies and database on supabase

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,6 +4,8 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
     id("org.jetbrains.kotlin.plugin.serialization")
+    id("com.google.devtools.ksp")
+    id("com.google.dagger.hilt.android")
 }
 
 val localProps = Properties().apply {
@@ -54,11 +56,11 @@ android {
 
 dependencies {
     // Kotlin Core
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.9.0")   // nâng lên
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.3")   // nâng lên phù hợp Kotlin 2.2.x
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.10.2")   // nâng lên
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.11.0")   // nâng lên phù hợp Kotlin 2.2.x
 
     // Lifecycle + Compose
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.7")     // nên nâng
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.10.0")     // nên nâng
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
@@ -69,7 +71,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
-    implementation("androidx.compose.material:material-icons-extended:1.7.0")   // nâng nếu có
+    implementation("androidx.compose.material:material-icons-extended:1.7.8")   // nâng nếu có
 
     // Supabase
     implementation(platform("io.github.jan-tennert.supabase:bom:3.6.0"))
@@ -79,6 +81,16 @@ dependencies {
     implementation("io.github.jan-tennert.supabase:realtime-kt")
 
     implementation("io.ktor:ktor-client-android:3.4.3")
+
+    // Hilt
+    implementation("com.google.dagger:hilt-android:2.59.2")
+    ksp("com.google.dagger:hilt-android-compiler:2.59.2")
+
+    // Retrofit
+    implementation ("com.google.code.gson:gson:2.14.0")
+    implementation ("com.squareup.retrofit2:retrofit:3.0.0")
+    implementation ("com.squareup.retrofit2:converter-gson:3.0.0")
+
 
     // Test
     testImplementation(libs.junit)
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,8 @@
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.compose) apply false
-    id("org.jetbrains.kotlin.plugin.serialization") version "2.2.21" apply false
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.3.21" apply false
+    id("com.google.dagger.hilt.android") version "2.59.2" apply false
+    id("com.google.devtools.ksp") version "1.9.22-1.0.17" apply false
+
 }
